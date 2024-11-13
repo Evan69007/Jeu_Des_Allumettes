10b92d1cd0d3e7301bbcb60a09db047cb8fa8065
Evan <evanaguttesrivelon@gmail.com>
2024-11-13 10:19:06 +0100

    jeu allumettes finit (c'est pas ultra beau mais le style c'est pas mon truc)

--- script.js
+++ script.js
@@ -12,34 +12,36 @@ const nb_joueur_button = document.querySelector('#nb_joueur_button');
 const reset_button = document.querySelector('#reset');
 const nb_restant = document.querySelector('#nb_restant');
 
-label.innerText = `Joueur ${joueur}, à vous de jouer`
-
 nb_joueur_button.addEventListener('click', () => {
 	header.innerText = ""
 	nb_joueur = input_joueur.value
-	nb_joueur_button.disabled = true
-	button.disabled = false
+	button.style.display = "inline-block"
+	input.style.display = "inline-block"
+	nb_joueur_button.style.display = "none"
+	input_joueur.style.display = "none"
 	input_joueur.value = ""
+	label.innerText = `Joueur ${joueur}, à vous de jouer`
 })
 
 button.addEventListener('click', () => {
 	nb_a_enlever = input.value
 	input.value = ""
-	nb_allumettes = enleve_Allumettes(nb_a_enlever)
-	if (nb_allumettes > 0)
+	if (nb_a_enlever < 7 && nb_a_enlever > 0)
 	{
-		update(nb_joueur)
-	}
-	else
-	{
-		header.innerText = `Bravo joueur ${joueur}! vous avez gagner`
-		label.innerText = ""
-		nb_restant.innerText = ""
-		input.style.display = "none"
-		input_joueur.style.display = "none"
-		button.style.display = "none"
-		nb_joueur_button.style.display = "none"
-		reset_button.style.display = "inline-block"
+		nb_allumettes = enleve_Allumettes(nb_a_enlever)
+		if (nb_allumettes > 0)
+		{
+			update(nb_joueur)
+		}
+		else
+		{
+			header.innerText = `Bravo joueur ${joueur}! Vous avez gagner`
+			label.innerText = ""
+			nb_restant.innerText = ""
+			input.style.display = "none"
+			button.style.display = "none"
+			reset_button.style.display = "inline-block"
+		}
 	}
 })
 
@@ -47,14 +49,10 @@ reset_button.addEventListener('click', () => {
 	nb_allumettes = 50
 	joueur = 1
 	reset_button.style.display = "none"
-	input.style.display = "inline-block"
 	input_joueur.style.display = "inline-block"
-	button.style.display = "inline-block"
 	nb_joueur_button.style.display = "inline-block"
-	button.disabled = true
-	nb_joueur_button.disabled = false
 	header.innerText = ""
-	label.innerText = `Joueur ${joueur}, à vous de jouer`
+	label.innerText = ""
 })
 
 function enleve_Allumettes(nb_a_enlever)
